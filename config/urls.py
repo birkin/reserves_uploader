cf3808157449778c80056ba6b58790b485a2d1f2
Birkin James Diana <birkin.diana@gmail.com>
2022-12-14 15:29:28 -0500

    adds uploader url.

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,10 @@ from reserves_uploader_app import views
 urlpatterns = [
     ## main ---------------------------------------------------------
     path( 'info/', views.info, name='info_url' ),
+    path( 'uploader/', views.uploader, name='uploader_url' ),
     ## other --------------------------------------------------------
     path( '', views.root, name='root_url' ),
-    path( 'admin/', admin.site.urls ),
+    # path( 'admin/', admin.site.urls ),
     path( 'error_check/', views.error_check, name='error_check_url' ),
     path( 'version/', views.version, name='version_url' ),
     # path( 'version2/', views.version2, name='version2_url' ),
