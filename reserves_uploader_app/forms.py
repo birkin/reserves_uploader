833d95f38d68f065871f42bab1a44c229612e5b4
Birkin James Diana <birkin.diana@gmail.com>
2022-12-18 19:50:52 -0500

    displays multiple bad-characters.

--- reserves_uploader_app/forms.py
+++ reserves_uploader_app/forms.py
@@ -11,28 +11,36 @@ log = logging.getLogger(__name__)
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
-    # file = forms.FileField( upload_to=settings.UPLOADS_DIR_PATH )
 
-    # def clean_filename(self):
     def clean(self):
         log.debug( 'starting clean()' )
         log.debug( f'self.cleaned_data, ``{self.cleaned_data}``' )
         filename = self.cleaned_data["file"].name
+        bad_characters = []
+        bad_character_string = ''
         for prohibited_character in settings.PROHIBITED_CHARACTERS:
             if prohibited_character in filename:
-                msg = f'error: prohibited filename character: ``{prohibited_character}``'
-                log.debug( msg )
-                raise ValidationError( msg )
+                bad_characters.append( prohibited_character )
+        if len( bad_characters ) > 0:
+            bad_character_string = ', '.join( bad_characters )
+            msg = f'error: prohibited filename characters: ``{bad_character_string}``'
+            log.debug( msg )
+            raise ValidationError( msg )
         # Always return a value to use as the new cleaned data, even if
         # this method didn't change it.
         log.debug( 'filename apparently valid' )
         return filename
 
-    # def clean_recipients(self):
-    #     data = self.cleaned_data['recipients']
-    #     if "fred@example.com" not in data:
-    #         raise ValidationError("You have forgotten about Fred!")
-
+    # def clean(self):
+    #     log.debug( 'starting clean()' )
+    #     log.debug( f'self.cleaned_data, ``{self.cleaned_data}``' )
+    #     filename = self.cleaned_data["file"].name
+    #     for prohibited_character in settings.PROHIBITED_CHARACTERS:
+    #         if prohibited_character in filename:
+    #             msg = f'error: prohibited filename character: ``{prohibited_character}``'
+    #             log.debug( msg )
+    #             raise ValidationError( msg )
     #     # Always return a value to use as the new cleaned data, even if
     #     # this method didn't change it.
-    #     return data
+    #     log.debug( 'filename apparently valid' )
+    #     return filename
