3301e1bd634b691bbbae4f66c396ed4b6870a29b
Birkin James Diana <birkin.diana@gmail.com>
2022-12-19 16:17:56 -0500

    redirects info-url to version-url for now.

--- reserves_uploader_app/views.py
+++ reserves_uploader_app/views.py
@@ -18,26 +18,15 @@ log = logging.getLogger(__name__)
 # main urls
 # -------------------------------------------------------------------
 
-# def uploader( request ):
-#     """ On GET, return the uploader page.
-#         On POST, process the uploaded file via django chunked-upload. """
-#     log.debug( 'starting' )
-#     if request.method == 'POST':
-#         log.debug( 'request.POST, ``%s``' % request.POST )
-#         log.debug( 'request.FILES, ``%s``' % request.FILES )
-#         return HttpResponse( 'Thanks for the upload!' )
-#     else:
-#         return render( request, 'reserves_uploader_app/uploader.html' )
-
 
 def info(request):
-    return HttpResponse( "zHello, world." )
+    return HttpResponseRedirect( reverse('version_url') )
 
 
 @ensure_csrf_cookie
 def uploader(request):
     log.debug( 'starting uploader()' )
-    # log.debug( f'inital incoming request.session.items(), ``{pprint.pformat(request.session.items())}``' )
+    log.debug( f'inital incoming request.session.keys(), ``{pprint.pformat(request.session.keys())}``' )
     if request.method == 'POST':
         log.debug( 'POST detected' )
         log.debug( f'request.POST, ``{pprint.pformat(request.POST)}``' )
@@ -45,31 +34,22 @@ def uploader(request):
         log.debug( f'request.session.items(), ``{pprint.pformat(request.session.items())}``' )
         assert request.session['msg'] == ''
         log.debug( 'about to instantiate form' )
-
         form = UploadFileForm(request.POST, request.FILES)
         log.debug( f'form.__dict__, ``{pprint.pformat(form.__dict__)}``' )
         if form.is_valid():
             log.debug( 'form is valid' )
             log.debug( f'form.cleaned_data, ``{pprint.pformat(form.cleaned_data)}``' )
             filename = uploader_helper.handle_uploaded_file( request.FILES['file'] )  # if duplicate, will have timestamp appended
-            # msg = 'File successfully uploaded'
             file_url = f'{settings.UPLOADS_DIR_URL_ROOT}/{filename}'
             msg = f'File uploaded; link: <a href="{file_url}">{filename}</a>'
-            # msg = f'File successfully uploaded; url: ``{file_url}``.'
             request.session['msg'] = msg
             log.debug( f'request.session.items(), ``{pprint.pformat(request.session.items())}``' )
-            # return render(request, 'templates/single_file.html', context)
         else:
             log.debug( 'form not valid' )
             log.debug( f'form.errors, ``{pprint.pformat(form.errors)}``' )
             log.debug( f'form.non_field_errors(), ``{pprint.pformat(form.non_field_errors())}``' )
             error_message = form.non_field_errors()[0]
             log.debug( f'error_message, ``{pprint.pformat(error_message)}``' )
-            # context = {'msg' : '<span style="color: red;">Form not valid</span>'}
-            # resp = render(request, 'templates/single_file.html', context)
-            # msg = f'<span style="color: red;">Form not valid, error: {error_message}</span>'
-            # msg = f'Form not valid, error: ``{error_message}``.'
-            # request.session['msg'] = msg
             request.session['msg'] = error_message
         log.debug( 'POST handled, about to redirect' )
         log.debug( f'at end of POST; request.session.keys(), ``{pprint.pformat(request.session.keys())}``' )
@@ -89,26 +69,51 @@ def uploader(request):
         resp = HttpResponseBadRequest( 'bad request' )
     return resp
 
-
-
-# def handle_uploaded_file(f):
-#     """ Handle uploaded file without overwriting pre-existing file. """
-#     log.debug( 'starting handle_uploaded_file()' )
-#     full_file_path = f'{settings.UPLOADS_DIR_PATH}/{f.name}'
-#     if os.path.exists( full_file_path ):
-#         log.debug( 'file exists; appending timestamp' )
-#         timestamp = datetime.datetime.now().strftime( '%Y-%m-%d_%H-%M-%S' )
-#         full_file_path = f'{settings.UPLOADS_DIR_PATH}/{f.name}_{timestamp}'
-#     log.debug( f'full_file_path, ``{full_file_path}``' )
-#     with open( full_file_path, 'wb+' ) as destination:
-#         log.debug( 'starting write' )
-#         for chunk in f.chunks():
-#             destination.write(chunk)
-#     log.debug( f'writing finished' )
-#     return
-
-
-
+# @ensure_csrf_cookie
+# def uploader(request):
+#     log.debug( 'starting uploader()' )
+#     log.debug( f'inital incoming request.session.keys(), ``{pprint.pformat(request.session.keys())}``' )
+#     if request.method == 'POST':
+#         log.debug( 'POST detected' )
+#         log.debug( f'request.POST, ``{pprint.pformat(request.POST)}``' )
+#         log.debug( f'request.FILES, ``{pprint.pformat(request.FILES)}``' )
+#         log.debug( f'request.session.items(), ``{pprint.pformat(request.session.items())}``' )
+#         assert request.session['msg'] == ''
+#         log.debug( 'about to instantiate form' )
+#         form = UploadFileForm(request.POST, request.FILES)
+#         log.debug( f'form.__dict__, ``{pprint.pformat(form.__dict__)}``' )
+#         if form.is_valid():
+#             log.debug( 'form is valid' )
+#             log.debug( f'form.cleaned_data, ``{pprint.pformat(form.cleaned_data)}``' )
+#             filename = uploader_helper.handle_uploaded_file( request.FILES['file'] )  # if duplicate, will have timestamp appended
+#             file_url = f'{settings.UPLOADS_DIR_URL_ROOT}/{filename}'
+#             msg = f'File uploaded; link: <a href="{file_url}">{filename}</a>'
+#             request.session['msg'] = msg
+#             log.debug( f'request.session.items(), ``{pprint.pformat(request.session.items())}``' )
+#         else:
+#             log.debug( 'form not valid' )
+#             log.debug( f'form.errors, ``{pprint.pformat(form.errors)}``' )
+#             log.debug( f'form.non_field_errors(), ``{pprint.pformat(form.non_field_errors())}``' )
+#             error_message = form.non_field_errors()[0]
+#             log.debug( f'error_message, ``{pprint.pformat(error_message)}``' )
+#             request.session['msg'] = error_message
+#         log.debug( 'POST handled, about to redirect' )
+#         log.debug( f'at end of POST; request.session.keys(), ``{pprint.pformat(request.session.keys())}``' )
+#         log.debug( f'at end of POST; request.session["msg"], ``{pprint.pformat(request.session["msg"])}``' )
+#         resp = HttpResponseRedirect( reverse('uploader_url') )  ## TODO, add message as querystring, then display it
+#     elif request.method == 'GET':
+#         log.debug( 'GET detected' )
+#         log.debug( f'request.session.items(), ``{pprint.pformat(request.session.items())}``' )
+#         ## get any session message ----------------------------------
+#         session_message = request.session.get('msg', '')
+#         log.debug( f'session_message, ``{session_message}``' )
+#         ## clear out session message --------------------------------
+#         request.session['msg'] = ''
+#         context: dict = uploader_helper.build_uploader_GET_context( session_message )
+#         resp = render( request, 'single_file.html', context )
+#     else:
+#         resp = HttpResponseBadRequest( 'bad request' )
+#     return resp
 
 
 # -------------------------------------------------------------------
