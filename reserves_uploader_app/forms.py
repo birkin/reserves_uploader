b48cce5012b18c3d5a0d8786fd45dad25311a41b
Birkin James Diana <birkin.diana@gmail.com>
2022-12-20 17:49:34 -0500

    handles hitting back-button and trying upload-file with no file selected.

--- reserves_uploader_app/forms.py
+++ reserves_uploader_app/forms.py
@@ -15,7 +15,12 @@ class UploadFileForm(forms.Form):
     def clean(self):
         log.debug( 'starting clean()' )
         log.debug( f'self.cleaned_data, ``{self.cleaned_data}``' )
-        filename = self.cleaned_data["file"].name
+        try:
+            filename = self.cleaned_data["file"].name
+        except:
+            msg = 'error: no file selected'
+            log.debug( msg )
+            raise ValidationError( msg )
         bad_characters = []
         bad_character_string = ''
         for prohibited_character in settings.PROHIBITED_CHARACTERS:
@@ -35,12 +40,19 @@ class UploadFileForm(forms.Form):
     #     log.debug( 'starting clean()' )
     #     log.debug( f'self.cleaned_data, ``{self.cleaned_data}``' )
     #     filename = self.cleaned_data["file"].name
+    #     bad_characters = []
+    #     bad_character_string = ''
     #     for prohibited_character in settings.PROHIBITED_CHARACTERS:
     #         if prohibited_character in filename:
-    #             msg = f'error: prohibited filename character: ``{prohibited_character}``'
-    #             log.debug( msg )
-    #             raise ValidationError( msg )
+    #             bad_characters.append( prohibited_character )
+    #     if len( bad_characters ) > 0:
+    #         bad_character_string = ', '.join( bad_characters )
+    #         msg = f'error: prohibited filename characters: {bad_character_string}'
+    #         log.debug( msg )
+    #         raise ValidationError( msg )
     #     # Always return a value to use as the new cleaned data, even if
     #     # this method didn't change it.
     #     log.debug( 'filename apparently valid' )
     #     return filename
+
+    ## end class UploadFileForm()
